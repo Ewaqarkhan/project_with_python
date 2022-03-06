9c891859deac0bc30000ff1ae5c6ffc5c1c569a2
waqarkhan <69013672+Ewaqarkhan@users.noreply.github.com>
2022-03-06 11:22:28 +0500

    create table for newtask

---
+++ NEWTASK.sql
@@ -0,0 +1,5 @@
+create table NEWTASK(
+ID INT           NOT NULL    IDENTITY    PRIMARY KEY,
+TASK_title VARCHAR(100)  NOT NULL,
+priority_title   VARCHAR(100)  NOT NULL,
+lable_title VARCHAR(100)  NOT NULL)
