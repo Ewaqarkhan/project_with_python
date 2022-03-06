c1062796b55b7bc637da6cf11b49e6059cc6958b
waqarkhan <69013672+Ewaqarkhan@users.noreply.github.com>
2022-03-06 11:05:00 +0500

    Update SIGNUP.sql

--- SIGNUP.sql
+++ SIGNUP.sql
@@ -1,5 +1,6 @@
 create table SIGN_UP(
-username INT           NOT NULL    IDENTITY    PRIMARY KEY,
+ID INT           NOT NULL    IDENTITY    PRIMARY KEY,
+username VARCHAR(100)  NOT NULL,
 email   VARCHAR(100)  NOT NULL,
 password_  VARCHAR(100)  NOT NULL,
 c_password VARCHAR(100)  NOT NULL) 
