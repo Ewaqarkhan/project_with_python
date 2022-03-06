6be2d8e0683140402bd6b18929e379861132a473
waqarkhan <69013672+Ewaqarkhan@users.noreply.github.com>
2022-03-06 10:45:31 +0500

    create table for login

---
+++ SIGNUP.sql
@@ -0,0 +1,5 @@
+create table SIGN_UP(
+username INT           NOT NULL    IDENTITY    PRIMARY KEY,
+email   VARCHAR(100)  NOT NULL,
+password_  VARCHAR(100)  NOT NULL,
+c_password VARCHAR(100)  NOT NULL) 
